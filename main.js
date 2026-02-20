478ebbf7fd5a8bf1675d0dbb3475a3997fd1131b
Panela-9R-ay2526 <panela.ap@obmontessori.edu.ph>
2026-02-20 13:55:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// fuction for Discount Button
+    function getDiscount(){
+        // Declare variables
+            let price = document.getElementById("budget").value;
+        // Compute for the 20% off discount
+            let discount = Number(price * 0.2).toFixed(2);
+            document.getElementById("area").value=  area;
+
+        // Display output using JS method
+        document.getElementById("brands").innerHTML  
+        }
