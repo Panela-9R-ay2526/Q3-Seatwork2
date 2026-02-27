88633507d02d13a1d1173775a653b7a9185ff2dd
Panela-9R-ay2526 <panela.ap@obmontessori.edu.ph>
2026-02-27 12:47:10 +0800

    Fix discount calculation and enhance brand display
    Refactor discount calculation and update HTML output for recommended brands.

--- main.js
+++ main.js
@@ -2,10 +2,38 @@
     function getDiscount(){
         // Declare variables
             let price = document.getElementById("budget").value;
-        // Compute for the 20% off discount
-            let discount = Number(price * 0.2).toFixed(2);
-            document.getElementById("area").value=  area;
-
+            let discount = Number(price * 0.2).toFixed(2); // Compute for the 20% off discount
         // Display output using JS method
-        document.getElementById("brands").innerHTML  
+            document.getElementById("brands").innerHTML =`
+            <div class="container" style="width: 30%;">
+                <h3>Recommended Brands</h3>
+                <button type="button" class="btn btn-warning mt-3">Acer</button>
+                <button type="button" class="btn btn-warning mt-3">Apple</button>
+                <button type="button" class="btn btn-warning mt-3">Lenovo</button>
+                <br>
+                <br>
+                <p>Eligible Discount:</P>
+                <h1><span class="badge bg-warning">${discount}</span></h1>
+            </div> `;
+
+    }
+
+    function getLocation(){
+            document.getElementById("location").innerHTML =`
+            <div class="container" style="width: 30%;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQOu117AkawylQF_POojkhD38UyC8cUcMtg&s">
+            <br>
+            <h3>Store Locations</h3>
+            <hr>
+            <li>
+                Greenhills: 3 Eisenhower St., Bgy. Greenhills, San Juan City, Metro Manila
+            </li>
+            <li> 
+               Sta. Ana: 2241 Pedro Gil St, Santa Ana, Manila, Metro Manila
+            </li>
+             <li> 
+               Angeles: MacArthur Hwy, Angeles, 2009 Pampanga
+            </li>
+            </div> `;
+            
         }
